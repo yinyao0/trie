cd006008a58b13e4f4649c5ac2a3d28263e07740
unknown <cdyinyao@CDYJY-YINYAO.360buyAD.local>
2015-08-12 15:52:01 +0800

    trie is ok

--- trie.go
+++ trie.go
@@ -7,24 +7,73 @@ import (
 
 type Text []byte
 
+
 type Node struct {
     Word    string
     Isword  bool
-    Child   []*Node
+    Child   map[string]*Node
+	size    int
 }
 
-/*func CreateTrie(data *string) {
-
+func CreateTrie(data []string) Node{
+     root := Node{Child : make(map[string]*Node)}
+     for  _, txt := range data {
+	    words := TexttoWord([]byte(txt))
+	    root.createNode(words,0)
+	 }
+	 return root
 }
 
-func (node *Node) GetOne(key string){
+func (node *Node) createNode(words []string,i int) {
+   if i>=len(words) {
+      return
+   }
+
+   
+   n := lookup(words[i],node)
+   if n != nil {
+       if i < len(words)-1 {
+	      i++
+	      n.createNode(words,i)
+	   }else if i == len(words)-1 {
+	      n.Isword = true
+	   }
+   }
 }
 
-func (node *Node) DeleteOne(key string){
+func lookup(word string,node *Node) *Node{
+   n, ok := node.Child[word]
+   if !ok {
+     n = new(Node)
+	 n.Word = word
+	 n.Child = make(map[string]*Node)
+	 node.Child[word] = n
+	 node.size++
+   }
+   return n
 }
 
-func (node *Node) InsertOne(key string){
-}*/
+func (node *Node) match(words []string,cursor int) int {
+    if words == nil{
+	   return 2
+	}
+	
+	w, ok := node.Child[words[cursor]]
+	if ok {
+	   if cursor == len(words)-1 {
+	      if w.Isword == true {
+		      return 0
+		  }
+		  if w.size>0{
+		      return 1
+		  }
+	   } else {
+	      cursor++
+	      return w.match(words,cursor)
+	   }
+	}
+	return 2
+}
 
 
 func TexttoWord(text Text) []string {
@@ -34,7 +83,6 @@ func TexttoWord(text Text) []string {
     i := 0
     for cursor < len(text) {
       p, size := utf8.DecodeRune(text[cursor:])
-      fmt.Printf("%c,%v\n",p,size)
       output[i] = string(p)
       i++
       cursor += size
@@ -42,12 +90,21 @@ func TexttoWord(text Text) []string {
     return output
 }
 
+func init() {
+   fmt.Printf("init\n")
+   //root := Node{Child : make(map[string]*Node)}
+}
 
 func main() {
    words := []string{"中国","美国","德国","法国","意大利"}
-   for _, word := range words {
-      fmt.Printf("%v\n",word)
-      key := TexttoWord([]byte(word))
-      fmt.Printf("%v\n",key)
+   root := CreateTrie(words)
+   //fmt.Printf("%v\n",root)
+   flag := root.match(TexttoWord([]byte("中国人")),0)
+   if flag==0{
+      fmt.Printf("total match\n")
+   } else if flag==2{
+      fmt.Printf("not match\n")
+   }else{
+      fmt.Printf("prefix match\n")
    }
 }
